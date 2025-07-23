fac1520385da0b7e75643221f883407927f8e0e2
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-07-23 15:47:03 +0300

    update

---
+++ modules/keycloak/main.tf
@@ -0,0 +1,34 @@
+
+resource "helm_release" "keycloak" {
+  name             = "keycloak"
+  repository       = "https://charts.bitnami.com/bitnami"
+  chart            = "keycloak"
+  namespace        = "default"
+  create_namespace = true
+  timeout          = 300
+  #depends_on = [helm_release.mariadb-galera]
+
+  set {
+    name  = "postgresql.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "postgresql.auth.username"
+    value = "keycloak"
+  }
+
+  set {
+    name  = "postgresql.auth.password"
+    value = "q1w2e3r4100@"
+  }
+
+  set {
+    name  = "postgresql.auth.database"
+    value = "keycloak"
+  }
+
+  set {
+    name  = "postgresql.auth.existingSecret"
+    value = ""
+  }
