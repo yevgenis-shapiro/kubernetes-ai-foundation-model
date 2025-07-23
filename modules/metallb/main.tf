97fc2c9757184bab252d4a3ecceaf9bdc8881f88
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-07-24 00:20:38 +0300

    update

--- modules/metallb/main.tf
+++ modules/metallb/main.tf
@@ -8,41 +8,15 @@ resource "helm_release" "metallb" {
   create_namespace = true
   timeout          = 300
 
-#  values = [
-#    <<EOF
-#podSecurityPolicy:
-#  enabled: false
-#EOF
-#  ]
-}
-
-resource "kubernetes_manifest" "ip_pool" {
-  manifest = {
-    apiVersion = "metallb.io/v1beta1"
-    kind       = "IPAddressPool"
-    metadata = {
-      name      = "default"
-      namespace = "metallb-system"
-    }
-    spec = {
-      addresses = ["172.18.255.100-172.18.255.120"]
-    }
-  }
-
-  depends_on = [helm_release.metallb]
-}
-
-resource "kubernetes_manifest" "l2_advertisement" {
-  manifest = {
-    apiVersion = "metallb.io/v1beta1"
-    kind       = "L2Advertisement"
-    metadata = {
-      name      = "advertisement"
-      namespace = "metallb-system"
-    }
-    spec = {} # empty is valid
-  }
-
-  depends_on = [helm_release.metallb]
+  values = [
+    <<EOT
+configInline:
+  address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+        - 172.18.255.100-172.18.255.120
+EOT
+  ]
 }
 
