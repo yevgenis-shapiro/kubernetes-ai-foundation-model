36b5b0210d1393720f9c9be70be4ed2d70c61a97
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-07-27 00:46:49 +0300

    update

--- modules/metallb/main.tf
+++ modules/metallb/main.tf
@@ -1,16 +1,3 @@
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-resource "kubernetes_namespace" "metallb" {
-  metadata {
-    name = "metallb-system"
-  }
-
-depends_on = [
-    kind_cluster.default
-  ]
-}
 
 resource "helm_release" "metallb" {
   name             = "metallb"
