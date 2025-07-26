1ad9a505c72ba1bc75f4dcaba318289558649961
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-07-27 00:49:39 +0300

    update

--- resources.tf
+++ resources.tf
@@ -12,20 +12,6 @@ provider "helm" {
   }
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-resource "kubernetes_namespace" "metallb" {
-  metadata {
-    name = "metallb-system"
-  }
-
-depends_on = [
-    kind_cluster.default
-  ]
-}
-
 module "metallb" {
   source = "./modules/metallb"
   #depends_on = [kind_cluster.default]
