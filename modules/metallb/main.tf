50f44b1fb2f416eaac654ac684b81d6f3f49159d
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-07-27 00:12:02 +0300

    update

--- modules/metallb/main.tf
+++ modules/metallb/main.tf
@@ -1,39 +1,49 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "metallb" {
+  metadata {
+    name = "metallb-system"
+  }
+}
 
 resource "helm_release" "metallb" {
   name             = "metallb"
   repository       = "https://metallb.github.io/metallb"
   chart            = "metallb"
-  namespace        = "metallb-system"
   version          = "0.14.8"
-  create_namespace = true
-  timeout          = 300
+  namespace        = kubernetes_namespace.metallb.metadata[0].name
+  create_namespace = false
   skip_crds        = false
+
+  depends_on = [kubernetes_namespace.metallb]
 }
 
-resource "kubernetes_manifest" "ip_pool" {
-  manifest = {
-    apiVersion = "metallb.io/v1beta1"
-    kind = "IPAddressPool"
-    metadata = {
-      name = "default"
-      namespace = "metallb-system"
-    }
-    spec = {
-      addresses = ["172.18.255.100-172.18.255.120"]
-    }
-  }
+resource "null_resource" "metallb_config" {
   depends_on = [helm_release.metallb]
-}
 
-resource "kubernetes_manifest" "l2_advertisement" {
-  manifest = {
-    apiVersion = "metallb.io/v1beta1"
-    kind = "L2Advertisement"
-    metadata = {
-      name = "advert"
-      namespace = "metallb-system"
-    }
-    spec = {}
+  provisioner "local-exec" {
+    command = <<EOT
+kubectl apply -f - <<EOF
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: default
+  namespace: metallb-system
+spec:
+  addresses:
+  - 172.18.255.100-172.18.255.120
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: default
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+  - default
+EOF
+EOT
   }
-  depends_on = [helm_release.metallb]
 }
