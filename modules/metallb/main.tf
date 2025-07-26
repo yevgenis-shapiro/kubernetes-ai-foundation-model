f25fddcb616d6cd5f352070e79480e458ccc87fb
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-07-26 23:29:48 +0300

    update

--- modules/metallb/main.tf
+++ modules/metallb/main.tf
@@ -1,24 +1,48 @@
-
-resource "helm_release" "metallb" {
-  name = "metallb"
-  repository = "https://metallb.github.io/metallb"
-  chart = "metallb"
-  namespace = "metallb-system"
-  version = "0.13.10"
-  create_namespace = true
-  timeout = 300
-  skip_crds        = false  # ✅ CRDs will be installed
-
-  values = [
-    <<EOT
-configInline:
-  address-pools:
-    - name: default
-      protocol: layer2
-      addresses:
-        - 172.18.255.100-172.18.255.120
-EOT
-  ]
+provider "kubernetes" {
+  config_path = "~/.kube/config" # or use config_context or in-cluster config
 }
 
+resource "kubernetes_namespace" "metallb" {
+  metadata {
+    name = "metallb-system"
+  }
+}
 
+resource "helm_release" "metallb" {
+  name             = "metallb"
+  repository       = "https://metallb.github.io/metallb"
+  chart            = "metallb"
+  namespace        = "metallb-system"
+  version          = "0.14.8"
+  create_namespace = true
+  timeout          = 300
+  skip_crds        = false
+}
+
+resource "kubernetes_manifest" "ip_pool" {
+  manifest = {
+    apiVersion = "metallb.io/v1beta1"
+    kind = "IPAddressPool"
+    metadata = {
+      name = "default"
+      namespace = "metallb-system"
+    }
+    spec = {
+      addresses = ["172.18.255.100-172.18.255.120"]
+    }
+  }
+  depends_on = [helm_release.metallb]
+}
+
+resource "kubernetes_manifest" "l2_advertisement" {
+  manifest = {
+    apiVersion = "metallb.io/v1beta1"
+    kind = "L2Advertisement"
+    metadata = {
+      name = "advert"
+      namespace = "metallb-system"
+    }
+    spec = {}
+  }
+  depends_on = [helm_release.metallb]
+}
