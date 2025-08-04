4ee92b57ed1e73f6292ed7a83087b6e2a65d3594
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-05 00:13:06 +0300

    update

--- modules/argo/main.tf
+++ modules/argo/main.tf
@@ -1,22 +1,14 @@
 
 resource "helm_release" "argocd" {
-  name  = "argocd"
-
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  namespace        = "argocd"
-  version          = "3.6.10"
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  version    = "8.2.5"               # <-- Latest Helm chart version
   create_namespace = true
-  timeout = 300
-
-  set {
-    name  = "controller.enableStatefulSet"
-    value = "true"
-  }
-
-  set {
-    name  = "configs.secret.argocdServerAdminPassword"
-    value = "$2a$10$lgcvwdvggWeLl1AN14NWsePcWQczWHRQH2eiUNL9w/gN6NaelDl.G"
-  }
-
+  timeout    = 300
+  values = [
+    file("./argo-values.yaml")
+  ]
 }
+
