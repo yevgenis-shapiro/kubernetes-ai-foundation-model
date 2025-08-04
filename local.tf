c8cf2ed78bb1a3dbbc51ccc9f530fb2c642e2726
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-04 23:00:33 +0300

    update

--- local.tf
+++ local.tf
@@ -1,34 +1,36 @@
 
-locals {
-    k8s_config_path = pathexpand("~/.kube/config")
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
 }
 
-resource "kind_cluster" "default" {
-    name            = var.cluster_name
-    node_image      = "kindest/node:${var.kind_version}"
-    kubeconfig_path = local.k8s_config_path
-    wait_for_ready  = true
+provider "null" {}
 
-  kind_config {
-      kind        = "Cluster"
-      api_version = "kind.x-k8s.io/v1alpha4"
+resource "null_resource" "install_k3s" {
+  provisioner "local-exec" {
+    command = "export K3S_EXEC_FLAGS='--disable=traefik --disable=local-storage' && bash ./modules/k3s/install_k3s.sh ${var.node_role} ${var.k3s_version}"
+  }
 
-      node {
-          role = "control-plane"
-
-          kubeadm_config_patches = [
-              "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
-          ]
-
-          extra_port_mappings {
-              container_port = 80
-              host_port      = 80
-          }
-          extra_port_mappings {
-              container_port = 443
-              host_port      = 443
-          }
-      }
+  triggers = {
+    always_run = "${timestamp()}"
 
   }
+}
+
+resource "null_resource" "k3s_config" {
+  provisioner "local-exec" {
+    command = "mkdir -p ~/.kube/ && cp -r /etc/rancher/k3s/k3s.yaml ~/.kube/config"
+  }
+  depends_on = [null_resource.install_k3s]
+}
+
+resource "null_resource" "k3s_status" {
+  provisioner "local-exec" {
+    command = "sleep 5 && systemctl status k3s | grep -v k3s 2>/dev/null"
+  }
+  depends_on = [null_resource.k3s_config]
 }
