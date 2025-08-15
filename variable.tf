ab4178328b82ef0b85acce5490cba8d56a7b6950
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-15 16:14:41 +0300

    update

--- variable.tf
+++ variable.tf
@@ -1,16 +1,29 @@
 
-variable "k3s_version" {
-  type    = string
-  default = "v1.29.3+k3s1"
+
+
+variable "kind_cluster_name" {
+  description = "The name of the Kind cluster"
+  type        = string
 }
 
-variable "node_role" {
-  type    = string
-  default = "server"
+variable "kind_cluster_config_path" {
+  description = "Path to save the Kind kubeconfig file"
+  type        = string
 }
 
-variable "disable_components" {
-  type    = string
-  default = "--disable traefik --disable local-storage"
+variable "k8s_version" {
+  description = "Kubernetes version to use for Kind nodes"
+  type        = string
+}
+
+variable "worker_count" {
+  description = "Number of worker nodes"
+  type        = number
+}
+
+variable "additional_control_planes_count" {
+  description = "Number of additional control planes for HA"
+  type        = number
+  default     = 2
 }
 
